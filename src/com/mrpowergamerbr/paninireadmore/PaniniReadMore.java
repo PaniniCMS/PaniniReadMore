81726c2744cf3bd0ef51f9e3ea183e55dc5433a5
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-04-23 11:35:12 -0300

    Initial commit

---
+++ src/com/mrpowergamerbr/paninireadmore/PaniniReadMore.java
@@ -0,0 +1,25 @@
+package com.mrpowergamerbr.paninireadmore;
+
+import com.paninicms.plugin.PaniniPlugin;
+import com.paninicms.plugin.event.GetPostEvent;
+import com.paninicms.utils.blog.Post;
+
+public class PaniniReadMore extends PaniniPlugin {
+	@Override
+	public void onEnable() {
+		System.out.println("PaniniReadMore by MrPowerGamerBR");
+	}
+	
+	@Override
+	public void onGetPost(GetPostEvent ev) {
+		for (Post post : ev.getLoadedPosts()) {
+			String htmlContent = post.content();
+			post.softMetadata().put("hasSummary", htmlContent.contains("<!--more-->"));
+			if (htmlContent.contains("<!--more-->")) {
+				post.softMetadata().put("summary", htmlContent.split("<!--more-->")[0]);
+			} else {
+				post.softMetadata().put("summary", htmlContent);
+			}
+		}
+	}
+}
