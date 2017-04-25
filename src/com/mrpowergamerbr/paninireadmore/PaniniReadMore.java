c33ed6900242579cd0f2b85435d801af98c74bb5
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-04-24 22:26:46 -0300

    Add a bunch of comments explaining what this plugin does

--- src/com/mrpowergamerbr/paninireadmore/PaniniReadMore.java
+++ src/com/mrpowergamerbr/paninireadmore/PaniniReadMore.java
@@ -11,14 +11,17 @@ public class PaniniReadMore extends PaniniPlugin {
 	}
 	
 	@Override
-	public void onGetPost(GetPostsEvent ev) {
-		for (Post post : ev.getLoadedPosts()) {
-			String htmlContent = post.content();
-			post.softMetadata().put("hasSummary", htmlContent.contains("<!--more-->"));
+	public void onGetPost(GetPostsEvent ev) { // When a post is loaded (getAllPosts(...))
+		for (Post post : ev.getLoadedPosts()) { // We are going to do a foreach on every loaded post
+			String htmlContent = post.content(); // Get the HTML content of it
+			post.softMetadata().put("hasSummary", htmlContent.contains("<!--more-->")); // And store on our softMetadata HashMap... "does this post contains a read more tag?"
+			// Templates can access the softMetadata map by using post.softMetadata.value
+			// For PaniniReadMore, we should use post.softMetadata.hasSummary on our template
 			if (htmlContent.contains("<!--more-->")) {
-				post.softMetadata().put("summary", htmlContent.split("<!--more-->")[0]);
+				post.softMetadata().put("summary", htmlContent.split("<!--more-->")[0]); // Now we are going to store EVERYTHING before our more tag to the "summary" variable
+				// We can use post.softMetadata.summary on our template to get the post's summary. Yay! We are done here.
 			} else {
-				post.softMetadata().put("summary", htmlContent);
+				post.softMetadata().put("summary", htmlContent); // And we also store the entire htmlContent if there isn't an read more tag
 			}
 		}
 	}
